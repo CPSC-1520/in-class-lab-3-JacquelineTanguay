0cc95ba1193a61fe044cd62fd2c2e1f2d22b529f
JacquelineTanguay <133934614+JacquelineTanguay@users.noreply.github.com>
2024-10-02 21:33:52 -0600

    Connected JS and HTML. Declared const and added EventListener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // Enter your code below.
+
+
+//console.log(document.querySelector('.javascript-resources'));
+//console.log(document.querySelector('#show-resources'));
+
+const getResourceButton = document.querySelector('#show-resources')
+
+getResourceButton.addEventListener('click', (onGetResource) => {
+    console.log('clicked!');
+})
